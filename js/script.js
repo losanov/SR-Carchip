79901ab1130f36aa2dd52a3edb31d79f4593b45d
losanov <losanov@ukr.net>
2021-03-12 00:45:51 +0200

    разметка выполнена

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function () {
       let mobImg = screen.width > maxMobWidth ? '' : 'mob-';
       body.style.backgroundImage = `url(../img/${mobImg}wallpapers-cars-0${imgCount}.jpg)`;
       imgCount++;
-    }, 2000);
+    }, 20000);
   }
 
   sliderBackGround(3, 767);
@@ -22,21 +22,22 @@ window.addEventListener('DOMContentLoaded', function () {
   const menuItems = [
     {link: '/', title: 'Вернуться на главную', name: 'Главная'},
     {link: 'diag', title: 'Что мы можем для вас сделать', name: 'Диагностика'},
-    {link: 'katalizator', title: 'Физическое и программное удаление катализаторов. Перевод на Евро2', name: 'Катализатор'},
-    {link: 'foto', title: 'Физическое и програмное удаление сажевого фильтра', name: 'Сажевый фильтр'},
-    {link: 'egr', title: 'Отключение клапана EGR (ЕГР)', name: 'EGR'},
-    {link: 'srs', title: 'Ремонт подушек безопасности', name: 'SRS AirBag'},
-    {link: 'immo-off', title: 'Решения без Иммобилайзера', name: 'Immo Off'},
-    {link: 'remont-ebu', title: 'Ремонт и замена блоков управления', name: 'Ремонт ЭБУ'},
     {link: 'tune', title: 'подменю', name: 'Чип Тюнинг'},
     {link: 'dsg', title: 'тюнинг коробок DSG', name: 'DSG'},
+    {link: 'remont-ebu', title: 'Ремонт и замена блоков управления', name: 'Ремонт ЭБУ'},
     {link: '', title: 'подменю Приборная панель', name: 'Приборная панель'},
-    {link: 'kompjuternaja-diagnostika-motor-testerom', title: 'Осцилограф', name: 'Диагностика мотор-тестером'},
-    {link: 'price', title: 'Цены', name: 'Цены'},
     {link: 'firmware', title: 'Прошивки на заказ', name: 'Прошивки на заказ'},
+    {link: 'price', title: 'Цены', name: 'Цены'},
     {link: 'contacts', title: 'как с нами связатся', name: 'Контакты'},
-    {link: 'about', title: 'Информация о компании', name: 'Среда обитания'},
-    {link: 'dop-uslugi', title: 'Доп Услуги', name: 'програмное удаление...'},
+
+    // {link: 'katalizator', title: 'Физическое и программное удаление катализаторов. Перевод на Евро2', name: 'Катализатор'},
+    // {link: 'foto', title: 'Физическое и програмное удаление сажевого фильтра', name: 'Сажевый фильтр'},
+    // {link: 'egr', title: 'Отключение клапана EGR (ЕГР)', name: 'EGR'},
+    // {link: 'srs', title: 'Ремонт подушек безопасности', name: 'SRS AirBag'},
+    // {link: 'immo-off', title: 'Решения без Иммобилайзера', name: 'Immo Off'},
+    // {link: 'kompjuternaja-diagnostika-motor-testerom', title: 'Осцилограф', name: 'Диагностика мотор-тестером'},
+    // {link: 'about', title: 'Информация о компании', name: 'Среда обитания'},
+    // {link: 'dop-uslugi', title: 'Доп Услуги', name: 'програмное удаление...'},
   ]
   
   
@@ -45,7 +46,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const {link, title, name} = item;
     return (`<li class='menu_list_item'>
           <span>
-            <a class='span_menu_item' href="${link}" title='${title}'>${name}</a>
+            <a class='span_menu_item' href="${link}" title='${title}'><h3>${name}</h3></a>
           </span>
         </li>`)
   }).join(' ');
